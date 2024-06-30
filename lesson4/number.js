d2021ec46c0e14cad1558a7d245af987abb6a7dd
yackohayuch4i <korobkatoect@gmail.com>
2024-06-30 19:42:36 +0300

    Update number.js

--- lesson4/number.js
+++ lesson4/number.js
@@ -1,1 +1,11 @@
-
+let number = prompt("Введіть трьох-значне число");
+if (number.length === 3 && isNaN(number))
+    {
+        let part = numper.split('');
+        let same = part[0] === part[1] && part[1] === part[2];
+        let anysame = part[0] === part[1] || part[1] === part[2] || part[0] === part[2];
+        console.log(`Чи всі цифри однакові? ${same ? 'yes' : 'no'} `);
+        console.log(`Чи повторюються деякі цифри? ${anysame ? 'yes' : 'no'}`);
+    } else{
+        alert("ВВедіть інше число");
+    }
