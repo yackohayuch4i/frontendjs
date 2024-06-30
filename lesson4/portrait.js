49fcd65b25d303f4e3e8b70ef48d7e88684a02dc
yackohayuch4i <korobkatoect@gmail.com>
2024-06-30 19:43:44 +0300

    Update portrait.js

--- lesson4/portrait.js
+++ lesson4/portrait.js
@@ -1,1 +1,31 @@
+//спочатку введемо необхідну інформацію
 
+const country = {
+    "київ" : "Україні" ,
+    "лондон" : "Великобританії" ,
+    "вашингтон" : "США" 
+};
+
+let datebd = prompt ("");
+if (datebd === null){
+    alert("Шкода , що ви не захотіли вести свій рік народження.")
+} else {
+    let city = prompt ("");
+    if (city === null){
+        alert("Шкода , що ви не захотіли вести .")
+    } else {
+    let sport = prompt ("");
+    if (sport === null){
+        alert("Шкода , що ви не захотіли вести .")
+     } else {
+        let years = new
+        Date().getFullYear();
+        let age = years - datebd;
+
+        let cityMessage = (country[city.toLowerCase()] ? `Ти живеш у місті ${country[city.toLowerCase()]}.`: 
+        `Ти живеш у  ${city}.` );
+        
+        alert (` : ${age} . \n${cityMessage}`)
+         }
+     }
+    }
