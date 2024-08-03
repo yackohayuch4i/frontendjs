60c986376741a12cf7e7f0c76b727e8dee50bb0e
yackohayuch4i <korobkatoect@gmail.com>
2024-08-03 12:35:00 +0300

    replecement of transliteration

---
+++ lesson5/resultnumber.js
@@ -0,0 +1,10 @@
+
+let i = 20;
+let result = '';
+
+while (i <= 30) {
+	result += i + ' ';
+  i += 0.5;
+}
+
+console.log(result.trim());
--- lesson5/valute.js
+++ lesson5/valute.js
@@ -2,6 +2,6 @@ let exchange = 26;
 for (let dollars = 10; 
     dollars <= 100;
     dollars += 10) {
-        let hryvnias = dollars * exchange;
-        console.log( `${dollars} dollars = ${hryvnias} hryvnias`);
+        let UAH = dollars * exchange;
+        console.log( `${dollars} dollars = ${UAH} UAH`);
 }
