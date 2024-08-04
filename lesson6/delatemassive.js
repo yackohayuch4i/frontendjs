7459ab4c8af77778ced0c8f1870317372eb159dd
yackohayuch4i <korobkatoect@gmail.com>
2024-08-04 13:07:36 +0300

    add function

---
+++ lesson6/delatemassive.js
@@ -0,0 +1,14 @@
+function remove (str , deletechars) {
+    let result = "";
+    for (let char of str) {
+        if (!deletechars.includes(char)) {
+            result += char;
+        }
+    }
+    return result;
+}
+
+let string = prompt ("Введіть рядок");
+let deletechar = prompt ("Введіть символи ,щоб видалити (без пробілів)").split("");
+let finalresult = remove(string, deletechar);
+console.log(`Результат: ${finalresult}`);
