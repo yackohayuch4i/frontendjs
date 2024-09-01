7b483cc477dbcd1a11866b6ad74089ed2c87d657
yackohayuch4i <korobkatoect@gmail.com>
2024-09-01 18:43:07 +0300

    Update average.js

--- lesson6/average.js
+++ lesson6/average.js
@@ -1,1 +1,11 @@
+function averageNum (arr) {
+    let numeration = arr.filter(element => typeof element === 'number');
+    if (numeration.length === 0) {
+        return 0;
+    }
+    let sum = numeration.reduce( (acc, curr) => acc + curr , 0);
+    return sum / numeration.length;
+}
 
+let someArray = [null , 6 ,2 , 4 ,8 , "number"];
+console.log(averageNum(someArray));
