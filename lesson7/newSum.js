d229663b54f4a3919fa1aec4a18dde30ae0b090c
yackohayuch4i <korobkatoect@gmail.com>
2024-09-02 08:06:10 +0300

    Update newSum.js

--- lesson7/newSum.js
+++ lesson7/newSum.js
@@ -1,1 +1,14 @@
+function newSum() {
+    let total = 0;
+    return function(num) {
+        total += num;
+        return total;
+    };
+}
 
+const sum = newSum();
+
+console.log(sum (4)); //вийде 4
+console.log(sum (6)); //вийде 10
+console.log(sum (10)); //вийде 20
+console.log(sum (7)); // вийде 27
