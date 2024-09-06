e8233aa348a05ad15141a611e5262382022e9ca1
yackohayuch4i <korobkatoect@gmail.com>
2024-09-06 14:23:23 +0300

    new code

---
+++ lesson10/infoUserCard.js
@@ -0,0 +1,12 @@
+let = {
+    name : "Mickhail",
+    surname : "Novak",
+    age : 25,
+    country : "Ukraine",
+
+    infoUserCard: function () {
+        console.log(`Ім'я користувача: &{this.name} , Фамілія користувача: &{this.surname} , Вік: &{this.age} ,Народився: &{this.country}`);
+        return;
+    }
+
+};
---
+++ lesson9/countTotalSalary.js
@@ -0,0 +1,9 @@
+let company = {
+
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600 }],
+    development: {
+    web: [{name: 'Peter', salary: 2000},
+     {name: 'Alex', salary: 1800 }],
+     internals: [{name: 'Jack', salary: 1300}]
+    }
+}
