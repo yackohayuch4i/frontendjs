cf531aeee04fd04f2e51d79712d192ac09c32412
yackohayuch4i <korobkatoect@gmail.com>
2024-09-25 16:04:08 +0300

    img

---
+++ lesson11/randomImages/randomim.js
@@ -0,0 +1,17 @@
+let image = [
+    '1.img',
+    '2.img',
+    '3.img',
+    '4.img',
+    '5.img',
+    '6.img',
+    '7.img'
+];
+
+function getRandomIm () {
+    const randomIndex = Math.floor(Math.random() * image.length);
+    return image[randomIndex];
+}
+
+const imgEl = document.querySelector('imageRandom');
+imgEl.src = `imagesforcode/$ {getRandomIm()}` ;
